1ad56fa37402da7fbe27c7394753527e531a792b
NadirZenith <2cb.md2@gmail.com>
2015-09-08 09:26:53 +0200

    ..

--- tpl/nzwpnewsletter-form.php
+++ tpl/nzwpnewsletter-form.php
@@ -1,6 +1,7 @@
 <div class="nzwpnewsletter">
     <div class="validation"></div>
     <form class="subscribe">
+        <div class="confirmation"></div>
         <label for="subscribe_email">
             <?php _e('Enter your email', 'nzwpnewsletter') ?>
             <input type="text" name="subscribe_email"/>
@@ -9,6 +10,7 @@
         <a class="switch"><?php _e('unsubscribe', 'nzwpnewsletter') ?></a>
     </form>
     <form class="unsubscribe" style="display: none">
+        <div class="confirmation"></div>
         <label for="unsubscribe_email">
             <?php _e('Unsubscribe from newsletter', 'nzwpnewsletter') ?>
             <input type="text" name="unsubscribe_email"/>
